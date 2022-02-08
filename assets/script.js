34ec1e8f3200e4f18ad3916dc4256173a31d8140
Pabs000 <pgarcia2000.pg@gmail.comgit config --global user.name Pablo>
2022-02-07 19:21:27 -0800

    commiting some changes

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Pablo Garcia" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
@@ -14,7 +14,7 @@ const credit = document.querySelector('#credit')
 const gbPlusBtn = document.querySelector('#add-gb')
 
 // Code to update name display
-credit.textContent = `Created by ${yourName}`
+credit.textContent = `Created by ${Pablo Garcia}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
 gbPlusBtn.addEventListener('click', function() {
@@ -23,5 +23,43 @@ console.log('Gingerbread + button was clicked!')
 
 // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
 })
-
 // TODO: Hook up event listeners for the rest of the buttons
+
+
+
+for gb( let i = 0; i < 1; i++){}
+console.log(i)
+const credit = document.querySelector('#credit')
+function( credit + '1')
+if (const gbPlusBtn = document.querySelector('#add-gb')(clicked)
+    (#add-gb == '1')
+if (const gbPlusBtn = document.querySelector('#add-gb')(clicked)
+    (#minus-gb == '-1')
+
+
+for cc (let i = 0; i < 1; i++){}
+console.log(i)
+const credit = document.querySelector('#credit')
+function( credit + '1')
+if (const gbPlusBtn = document.querySelector('#add-cc')(clicked)
+    (#add-cc == '1')
+if (const gbPlusBtn = document.querySelector('#add-cc')(clicked)
+    (#minus-cc == '-1')
+
+
+
+for sugar( let i = 0; i < 1; i++){}
+console.log(i)
+const credit = document.querySelector('#credit')
+function( credit + '1')
+if (const gbPlusBtn = document.querySelector('#add-sugar')(clicked)
+    (#add-sugar == '1')
+if (const gbPlusBtn = document.querySelector('#add-gb')(clicked)
+    (#minus-sugar == '-1')
+
+
+    let document.querySelector('#qty-total') = total document.querySelector('#qty-gb') +('#qty-cc')+('#qty-sugar')
+    th id="qty-total">0 = total<
+
+
+
